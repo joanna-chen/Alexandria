ccfbc66c09d708bf83c2d8c158c6aacf2212e373
joanna-chen <joannachen.x@gmail.com>
2016-12-17 10:51:23 -0500

    last commit

--- app/src/main/java/com/joanna/alexandria/ImageAdapter.java
+++ app/src/main/java/com/joanna/alexandria/ImageAdapter.java
@@ -5,6 +5,7 @@ package com.joanna.alexandria;
  */
 
 import android.app.AlertDialog;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.view.LayoutInflater;
@@ -56,6 +57,31 @@ public class ImageAdapter extends BaseAdapter {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         View gridView;
+
+        //////////////////////////
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(DownloadManager.Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        ////////////////////////
 
         if (convertView == null) {
 
